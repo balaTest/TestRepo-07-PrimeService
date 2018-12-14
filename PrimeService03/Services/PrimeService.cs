7729f19007e5160f3c79dab6828916eb856ac7d6
Bala Chirtsabesan <balaTest@live.com>
2018-12-14 07:04:51 -0800

    Handle 0, 1 and negative numbers

--- PrimeService03/Services/PrimeService.cs
+++ PrimeService03/Services/PrimeService.cs
@@ -9,6 +9,11 @@ namespace PrimeService03.Services
     {
         public bool IsPrime(int candidate)
         {
+            if (candidate < 2)
+            {
+                return false;
+            }
+
             for (int divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
             {
                 if (candidate % divisor == 0)
